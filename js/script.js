b67900b2f247d48ac3608debd49d5eb4aa8d8671
Jessica Giehl <sumdian@gmail.com>
2018-04-26 15:13:55 -0400

    test code handling for json objects

--- js/script.js
+++ js/script.js
@@ -20,27 +20,31 @@ function searchResults(){
     })
     .then(function(myJson) {
       console.log(myJson);
-      // if (myJson.hasOwnProperty('error')){
-      //   var error = `<p class="error">${myJson.message}</p>`;
-      //   document.querySelector('section').insertAdjacentHTML('afterbegin', error);
-      // } else {
-      //
-      //   //create a string to store adjacentHTML
-      //   var output = `<ul class="photogallery">`;
-      //   //create list items for 9 albums
-      //   for (var i = 0; i < 9; i++) {
-      //     output +=
-      //     ` <li>
-      //         <img src="${myJson.topalbums.album[i].image[3]['#text']}">
-      //         <h3>${myJson.topalbums.album[i].name}</h3>
-      //         <p>${myJson.topalbums.album[i].artist.name}</p>
-      //         <p><a href="${myJson.topalbums.album[i].url}">${myJson.topalbums.album[i].name}</a></p>
-      //       </li>`
-      //   }
-      //   output += `</ul>`;
-      //
-      //   document.querySelector('section').insertAdjacentHTML('afterbegin', output);
-      // }
+      if (myJson.hasOwnProperty('error')){
+        var error = `<p class="error">${myJson.message}</p>`;
+        document.querySelector('section').insertAdjacentHTML('afterbegin', error);
+      } else {
+
+        //create a string to store adjacentHTML
+        var output = `<ul class="photogallery">`;
+        //create list items for 9 albums
+        for (var i = 0; i < 9; i++) {
+          output +=
+          `<li>
+            <img src="${myJson.results[i].urls.regular}"
+                 srcset="${myJson.results[i].urls.regular} 1024w, ${myJson.results[i].urls.regular} 640w, ${myJson.results[i].urls.regular} 320w"
+                 sizes="(min-width: 36em) 33.3vw, 100vw"
+                 alt="A photo of water" />`
+              // <img src="${myJson.topalbums.album[i].image[3]['#text']}">
+              // <h3>${myJson.topalbums.album[i].name}</h3>
+              // <p>${myJson.topalbums.album[i].artist.name}</p>
+              // <p><a href="${myJson.topalbums.album[i].url}">${myJson.topalbums.album[i].name}</a></p>
+            // </li>`
+        }
+        output += `</ul>`;
+
+        document.querySelector('section').insertAdjacentHTML('afterbegin', output);
+      }
     })
     .catch(error => console.error(error));
 }
